a51e274c13ac927957b7f97d34f8031730b2fed0
Arda Iskender <iskenderarda@gmail.com>
2023-01-01 13:36:55 +0300

    v1.2
    - Cleaned some codes and project hierarchy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+
     public float moveSpeed = 2f;
     public float maxSpeed = 6f;
-    private Rigidbody playerRb;
+
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        
+        playerRb = GetComponent<Rigidbody>();  
     }
+
     void Update()
+    {
+        MovePlayer();
+    }
+
+    // Moves the player based on arrow key input
+    void MovePlayer()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        // Limit the velocity of the player
         if (playerRb.velocity.magnitude > maxSpeed)
         {
             playerRb.velocity = Vector3.ClampMagnitude(playerRb.velocity, maxSpeed);
         }
 
+        // When user presses two axis at the same time reduce the speed
         if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
         {
             moveSpeed = 1.5f;
@@ -34,6 +44,7 @@ public class PlayerController : MonoBehaviour
             moveSpeed = 2f;
             playerRb.AddForce(Vector3.forward * moveSpeed * verticalInput);
             playerRb.AddForce(Vector3.right * moveSpeed * horizontalInput);
-        }  
+        }
     }
+
 }
